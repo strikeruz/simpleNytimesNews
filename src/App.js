84549f4bc1295f20c2223257bbedfffd4659108a
Nursulton <nk@77projects.com>
2021-03-24 17:54:17 +0500

    added files config

---
+++ src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>TEST</h1>
+        { `test` }
+      </header>
+    </div>
+  );
+}
+
+export default App;
